a9c68789227382b76cdb6f56d6e5f874575b3056
Ioana Nes <ioanapopa095@gmail.com>
2020-03-10 17:33:04 +0200

    update

--- src/main/java/com/api/resources/JaxRSController.java
+++ src/main/java/com/api/resources/JaxRSController.java
@@ -1,5 +1,14 @@
 package com.api.resources;
 
+import com.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
@@ -7,13 +16,22 @@ import javax.ws.rs.core.Response;
 @Path("/")
 public class JaxRSController {
 
+    @Autowired
+    private HelloService helloService;
+
     @GET
     @Path("helloMessage")
     public Response sayHello() {
 
         String output = "Hello world!";
-
         return Response.ok(output).status(200).build();
+    }
 
+    @GET
+    @Path("welcome")
+    public String welcome() {
+
+        String message = helloService.welcome();
+        return message;
     }
 }
